537611d2d504ef7eafc4662d56dc7ab60efec2fc
Even <chenyuwen1@meizu.com>
2022-08-29 19:27:15 +0800

    [Even] Change the clang-11 to build the bcc.

--- bcc/build-bcc.sh
+++ bcc/build-bcc.sh
@@ -5,8 +5,8 @@ spath=$( cd "$(dirname "$0")" ; pwd -P )
 cd $spath
 
 rm -rf build && mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-6.0 -DCMAKE_CXX_COMPILER=clang++-6.0
-make -j4
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_COMPILER=clang-11 -DCMAKE_CXX_COMPILER=clang++-11
+make -j8
 make install
 cd ..
 rm -rf build
